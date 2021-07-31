ddf8977cea1c3eb1f621d78d306944f03b4d6b26
akolatch <aaron.kolatch@gmail.com>
2021-07-30 21:47:52 -0400

    delete ticker

--- server/src/controllers/ticker.ts
+++ server/src/controllers/ticker.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
-import { Portfolios, Tickers } from '../models';
+import { Tickers } from '../models';
 import { Status } from '../constants';
-import { invalidNumber } from './helpers/invalidNumber';
 import { ITickerUpdates } from '../types';
 import { invalidUpdate } from './helpers/invalidUpdate';
 
@@ -31,5 +30,20 @@ export const ticker = {
       res.sendStatus(Status.Error);
     }
   },
-  delete: async (req: Request, res: Response) => {},
+  delete: async (req: Request, res: Response) => {
+    const id = req.params.id;
+    try {
+      const exists = await Tickers.findByPk(id);
+      if (exists === null) {
+        res.status(Status.NotFound).json({ message: 'Portfolio not found' });
+        return;
+      }
+
+      await Tickers.destroy({ where: { id } });
+      res.sendStatus(Status.Accepted);
+    } catch (err) {
+      console.log('error at ticker.delete: ', err);
+      res.sendStatus(Status.Error);
+    }
+  },
 };
