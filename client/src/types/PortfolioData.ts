2a7025483798e7303c7e584de3fae8a8e0819fb7
akolatch <aaron.kolatch@gmail.com>
2021-07-31 20:36:55 -0400

    code cleanup

--- client/src/types/PortfolioData.ts
+++ client/src/types/PortfolioData.ts
@@ -8,14 +8,6 @@ export interface NewTicker extends Ticker {
   pricePaid: number;
 }
 
-// export type TickerData = {
-//   id: number;
-//   symbol: string;
-//   currentPrice: string;
-//   numShares: number;
-//   pricePaid: string;
-//   purchaseDate: string;
-// };
 export interface TickerData extends Ticker {
   id: number;
   pricePaid: string;
